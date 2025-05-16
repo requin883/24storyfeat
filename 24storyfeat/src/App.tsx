ebd28dcdc136e27f03d4479ebe0ffdac225b19d2
Javi883 <requin883@gmail.com>
2025-05-15 21:01:17 -0400

    FC

---
+++ 24storyfeat/src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='w-md text-white mx-auto h-screen justify-items-center grid text-center auto-rows-min bg-black p-8'>
+        <h1 className=" font-bold self-start text-2xl">24H Story</h1>
+        <div className="container m-8 self-start border-white border-2 rounded-sm w-full p-2 border-solid grid justify-items-center auto-cols-min">
+          <div className="border-white border-1 rounded-full w-10 h-10 grid place-items-center">
+            <p className="font-bold text-xl">+</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
